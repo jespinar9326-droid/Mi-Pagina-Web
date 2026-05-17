8bbdf4e5f293d6c399360cd9f8c74a6d2b9c0dd7
jespinar9326-droid <jespinar9326@utm.edu.ec>
2026-05-17 13:28:50 -0500

    Update settings.py

--- mi_pagina_web/settings.py
+++ mi_pagina_web/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'mi_pagina_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/jeremyespinar/Mi-Pagina-Web/db.sqlite3',
     }
 }
+
 
 
 # Password validation
